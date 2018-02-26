e6d24b4c2b4a37a20e05ee6dae7b1c82afffa6ca
gpulido <gabriel.pulidodetorres@gmail.com>
2018-02-27 00:04:37 +0100

    Start working on serial communication

---
+++ selve/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import time
+import serial
+
+ser = serial.Serial(
+    port='/dev/ttyUSB1',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS
+)
