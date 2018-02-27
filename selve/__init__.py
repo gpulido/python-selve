66be0efd198f29b7128cdd344a23008bbc6d13f0
gpulido <gabriel.pulidodetorres@gmail.com>
2018-02-27 16:06:45 +0100

    start modeling the xml commands

--- selve/__init__.py
+++ selve/__init__.py
@@ -3,6 +3,8 @@
 import time
 import serial
 
+from enum import Enum
+
 ser = serial.Serial(
     port='/dev/ttyUSB1',
     baudrate=115200,
@@ -10,3 +12,49 @@ ser = serial.Serial(
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS
 )
+
+
+
+
+class ParameterType(Enum):
+    INT = "int"
+    STRING = "string"
+    BASE64 = "base64"
+
+class DeviceType(Enum):
+    NOTYPE = 0
+    BLIND = 1
+    AWNING = 2
+    AWNING2 = 3
+    DIMMER = 4
+    DIMMER2 = 5
+    DIMMER3 = 6
+    DIM = 7
+    HEAT = 8
+    HEAT2 = 9
+    COOLING = 10
+    GATEWAY = 11
+
+
+class MethodCall:
+
+    def __init__(self, method_name, parameters):
+        self.method_name = method_name
+        self.parameters = parameters
+
+    def serializeToXML(self):
+        xmlstr = "<methodCall>"
+        xmlstr += "<methodName>"+self.method_name+"</methodName>"
+        for typ, val in self.parameters:
+            xmlstr+="<{0}>{1}</{0}>".format(typ, val)
+        xmlstr+= "</methodCall>"
+        return smlstr
+
+class MethodResponse:
+        
+    def __init__(self, str):
+        self.str = str
+        //TODO: deserialize str
+        
+
+    
