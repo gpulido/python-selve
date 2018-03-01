ae9905854b0e2ee6f6c4acd8748a5a25faeaded9
gpulido <gabriel.pulidodetorres@gmail.com>
2018-03-01 17:22:20 +0100

    Add serial hardware communication file

--- selve/__init__.py
+++ selve/__init__.py
@@ -7,14 +7,23 @@ from enum import Enum
 
 from .XML2py import deserialize
 
-ser = serial.Serial(
-    port='/dev/ttyUSB1',
-    baudrate=115200,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS
-)
 
+
+def sendCommand(methodCall):
+
+    ser = serial.Serial(
+        port='/dev/ttyUSB1',
+        baudrate=115200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS
+    )
+    ser.write(methodCall.serializeToXML)
+    
+    while True: 
+        response = ser.readline()
+
+    ser.close()
 
 
 
