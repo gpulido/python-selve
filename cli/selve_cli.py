490d27bddf8c80b92588977f1517e2ec6c9c3696
gpulido <gabriel.pulidodetorres@gmail.com>
2018-03-13 07:39:40 +0100

    add automatic flag to cli

--- cli/selve_cli.py
+++ cli/selve_cli.py
@@ -16,15 +16,15 @@ def action(args):
         sys.exit("The device is not registered on the gateway")
 
     if args.command == 'stop':
-        gat.devices[args.iveoId].stop()
+        gat.devices[args.iveoId].stop(args.automation)
     elif args.command == 'up':
-        gat.devices[args.iveoId].moveUp()
+        gat.devices[args.iveoId].moveUp(args.automation)
     elif args.command == 'down':
-        gat.devices[args.iveoId].moveDown()
+        gat.devices[args.iveoId].moveDown(args.automation)
     elif args.command == 'pos1':
-        gat.devices[args.iveoId].moveIntermediatePosition1()
+        gat.devices[args.iveoId].moveIntermediatePosition1(args.automation)
     elif args.command == 'pos2':
-        gat.devices[args.iveoId].moveIntermediatePosition2()
+        gat.devices[args.iveoId].moveIntermediatePosition2(args.automation)
 
 
 parser = argparse.ArgumentParser(prog='selve-cli')
@@ -35,7 +35,8 @@ parser_list.set_defaults(func=list_devices)
 
 parser_action = subparsers.add_parser('action', help="Command to execute over the device")
 parser_action.add_argument("command", choices = ['stop', 'up', 'down', 'pos1', 'pos2'], default = 'stop')
-parser_action.add_argument("iveoId", type=int, help="IveoId / channel where the device is registered'")
+parser_action.add_argument("iveoId", type=int, help="IveoId / channel where the device is registered")
+parser_action.add_argument("--automation", type=bool, default=False, help="Set to true if the command has to be executed as an automation mecanism to not override manual interaction with the device")
 parser_action.set_defaults(func=action)
 
 args = parser.parse_args()
